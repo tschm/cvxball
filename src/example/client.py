582f34d4cfa4016f74793d13f402384d4695ae8e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-20 10:38:28 +0400

    85 update client using context (#86)
    * client with context
    
    * no pydantic yet

--- src/example/client.py
+++ src/example/client.py
@@ -5,17 +5,15 @@ from np.flight import Client
 
 def main():
     # Connect to the server
-    client = Client("grpc+tcp://127.0.0.1:8815")
+    with Client("grpc+tcp://127.0.0.1:8815") as client:
+        # Example data
+        # The server is expecting a dictionary of numpy arrays
+        data = {"input": np.random.randn(2000, 10)}
 
-    # Example data
-    # The server is expecting a dictionary of numpy arrays
-    data = {"input": np.random.randn(2000, 10)}
+        # The server will return a dictionary of numpy arrays
+        results = client.compute(command="test", data=data)
 
-    # The server will return a dictionary of numpy arrays
-    results = client.compute(command="test", data=data)
-
-    logger.info(f"Results: {results.keys()}")
-    client.close()
+        logger.info(f"Results: {results.keys()}")
 
 
 if __name__ == "__main__":
